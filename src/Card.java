136773652676ddb81cfcd29ed769070c49a26c2a
Jacky11221 <144474368+Jacky11221@users.noreply.github.com>
2025-06-03 15:32:20 -0400

    balancing
    wrong class

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+
 public class Card {
     private final String name;
     private final int attackBuff;
@@ -12,9 +13,23 @@ public class Card {
     private static final ArrayList<Card> rareCards = new ArrayList<>();
     private static final ArrayList<Card> goodCards = new ArrayList<>();
 
+    private static final String blue = "\u001B[32m";
+    private static final String yellow = "\u001B[33m";
+    private static final String purple = "\u001B[35m";
+    private static final String reset_colour = "\u001B[0m";
+
 
     public Card(String name, int attackBuff, int defenseBuff, int healthRecovery, int rarity){
-        this.name = "{" + name + "}";
+        if (rarity==1) {
+            this.name = blue + "{" + name + "}" + reset_colour;
+        }
+        else if (rarity==2) {
+            this.name = yellow +"{" + name + "}" + reset_colour;
+        }
+        else {
+            this.name = purple + "{" + name + "}" + reset_colour;
+        }
+
         this.attackBuff = attackBuff;
         this.defenseBuff = defenseBuff;
         this.healthRecovery = healthRecovery;
@@ -47,6 +62,8 @@ public class Card {
         healthRecoveryGone = true;
     }
 
+//    private static String getCardColor()
+
     public int getRarity() {return rarity;}
 
     public String getEffects() {
@@ -70,44 +87,44 @@ public class Card {
 
     public static void generateCards(){
         //(Mostly) Attack related buffs
-        normalCards.add(new Card("Spoofing", 4, 0, 0, 1));
-        normalCards.add(new Card("Social Engineering", 5, 0, 0, 1));
-        normalCards.add(new Card("Denial of Service", 7, 0, 0, 1));
+        normalCards.add(new Card("Spoofing (good)", 4, 0, 0, 1));
+        normalCards.add(new Card("Social Engineering (good)", 5, 0, 0, 1));
+        normalCards.add(new Card("Denial of Service (good)", 7, 0, 0, 1));
 
-        rareCards.add(new Card("Distributed Denial of Service", 14, 0, 0, 2));
-        rareCards.add(new Card("XSS", 16, 2, 0, 2));
-        rareCards.add(new Card("SQLi", 16, 0, 2, 2));
+        rareCards.add(new Card("Distributed Denial of Service (good)", 14, 0, 0, 2));
+        rareCards.add(new Card("XSS (good)", 16, 2, 0, 2));
+        rareCards.add(new Card("SQLi(good)", 16, 0, 2, 2));
 
-        goodCards.add(new Card("Zero-Day Exploit", 25, 5, 0, 3));
-        goodCards.add(new Card("Focused Attack Vector", 40, -40, -20, 3));
+        goodCards.add(new Card("Zero-Day Exploit (good)", 25, 5, 0, 3));
+        goodCards.add(new Card("Focused Attack Vector   (Good)", 40, -40, -20, 3));
 
 
         //(Mostly) Defense related buffs
-        normalCards.add(new Card("Secure Configuration", 0, 2, 0, 1));
-        normalCards.add(new Card("Antivirus Software", 0, 3, 0, 1));
-        normalCards.add(new Card("Building Security I", 0, 2, 2, 1));
-        normalCards.add(new Card("Post-Incident Recovery", 0,0,15,1));
+        normalCards.add(new Card("Secure Configuration (Good)", 0, 2, 0, 1));
+        normalCards.add(new Card("Antivirus Software (Good)", 0, 3, 0, 1));
+        normalCards.add(new Card("Building Security I (UYAY)", 0, 2, 2, 1));
+        normalCards.add(new Card("Post-Incident Recovery (YAY)", 0,0,15,1));
 
 
-        rareCards.add(new Card("Security Training", 0, 8, 0, 2));
-        rareCards.add(new Card("Network Monitoring", 0, 2, 7, 2));
-        rareCards.add(new Card("Building Security II", 0, 6, 6, 2));
+        rareCards.add(new Card("Security Training (good)", 0, 8, 0, 2));
+        rareCards.add(new Card("Network Monitoring (good)", 0, 2, 7, 2));
+        rareCards.add(new Card("Building Security II (good)", 0, 6, 6, 2));
 
-        goodCards.add(new Card("Advanced Firewalls and Protection", -10, 15, 0, 3));
-        goodCards.add(new Card("Data Backup", 0, 5, 30, 3));
-        goodCards.add(new Card("Building Security III", 0, 15, 15, 3));
+        goodCards.add(new Card("Advanced Firewalls and Protection (kind of good?)", -10, 25, 0, 3));
+        goodCards.add(new Card("Data Backup (good)", 0, 5, 30, 3));
+        goodCards.add(new Card("Building Security III (good)", 0, 15, 15, 3));
 
 
         //Suffer lmao
-        normalCards.add(new Card("Insecure Wifi", 0, -3, 0, 1));
-        normalCards.add(new Card("Down Internet", -3, 0, 0, 1));
-        normalCards.add(new Card("Malfunctioning Hardware", -2, -2, -2, 1));
+        normalCards.add(new Card("Insecure Wifi (damn)", 2, -3, 0, 1));
+        normalCards.add(new Card("Down Internet (:()", -3, 3, 0, 1));
+        normalCards.add(new Card("Malfunctioning Hardware (not good)", -1, -1, -2, 1));
 
-        rareCards.add(new Card("Weak Security", 0, -8, -2, 2));
-        rareCards.add(new Card("Weak Door", 0, -10, 0, 2));
-        rareCards.add(new Card("Eradication of Exploitations", -10, 0, 0, 2));
+        rareCards.add(new Card("Weak Security (bad)", 0, -5, -2, 2));
+        rareCards.add(new Card("Weak Door (bad)", 0, -8, 0, 2));
+        rareCards.add(new Card("Eradication of Exploitations (bad)", -5, 0, 0, 2));
 
-        goodCards.add(new Card("Coursera", -20, -20, -20, 3));
-        goodCards.add(new Card("Awareness Void", 0, -1000000, 10, 3));
+        goodCards.add(new Card("Coursera (REALLY bad)", -20, -20, -20, 3));
+        goodCards.add(new Card("Awareness Void (oops no defense)", 0, -1000000, 100, 3));
     }
 }
